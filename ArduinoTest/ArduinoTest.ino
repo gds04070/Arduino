b0efce4eaa5d6374555b94c61930ff62ce4dac69
gds04070 <gds04070@gmail.com>
2018-12-12 03:21:37 +0900

    Complete

--- ArduinoTest/ArduinoTest.ino
+++ ArduinoTest/ArduinoTest.ino
@@ -48,24 +48,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(btSerial.available()){
-      //수신받은 데이터 저장
       byte data = btSerial.read();
-      //수신된 데이터 시리얼 모니터로 출력
       Serial.write(data);
-      //수신받은 데이터를 버퍼에 저장
       buffer[bufferPosition++] = data;
 
     if(data = 'F'){
-      //LED가 꺼있을 경우 LED를 킨다
       if(temp == 0){
         digitalWrite(6, HIGH);
         temp = 1;
-        }else{ //LED가 켜져 있을 경우 LED를 끈다
+        }else{ 
           digitalWrite(6,LOW);
           temp = 0;
           }
       } 
-      //문자열 종료 표시
       if(data == '\n'){
          buffer[bufferPosition] = '\0';
          bufferPosition = 0;
@@ -74,16 +69,6 @@ void loop() {
 
    calculateDuration();
    
-//  btSerial.print("test-");
-//  btSerial.println(3);
-//  delay(1000);
-
-//데이터 송신
-  if(Serial.available()){
-    btSerial.write(Serial.read());
-    //byte Senddata = "6";
-    //btSerial.write(Senddata);
-    }
   
 }
 
@@ -101,7 +86,7 @@ void calculateDuration(){
     if(n >= 10){
       average = sum / 10;
       Serial.println(average);
-      btSerial.println(average);
+      btSerial.println(average); //안드로이드로 보냄
       n = 0;
       sum = 0;
       }
